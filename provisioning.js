70e9474114416a50bb4872ea05eb345b46dbb1e6
Igor Veselinovic <igor@veselinovic.net>
2019-01-20 13:55:04 -0500

    Add provisioning script and update package.json
    Also package-lock is updated. The provisioning script just adds
    products. If it runs more than once, it provisions duplicates. This
    behaviour needs to get fixed eventually.

---
+++ provisioning.js
@@ -0,0 +1,24 @@
+const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+const path = require('path');
+
+/* TODO: Make URL a config variable */
+MongoClient.connect('mongodb://localhost:27017/exampleDb', { 'useNewUrlParser': true }, async function(err, client) {
+  if (err) {
+    console.error(err);
+  }
+
+  const dataString = fs.readFileSync(path.join(__dirname, 'provisioningData.json'), 'utf-8');
+  const { data } = JSON.parse(dataString);
+
+  console.log('Connected to MongoDB instance');
+  const db = client.db('marketplace-db');
+
+  try {
+    const collection = await db.collection('inventory');
+    await collection.insertMany(data, { 'w': 1 });
+    await client.close();
+  } catch (error) {
+    console.error(error);
+  }
+});
