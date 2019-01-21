bac2dc71e9035b3ef609d38fd038b20108473095
Igor Veselinovic <igor@veselinovic.net>
2019-01-21 03:25:51 -0500

    Add documentation to route class

--- route.js
+++ route.js
@@ -1,6 +1,14 @@
-'user strict';
+'use strict';
 
+/**
+ * Class for managing API enpoints/paths
+ */
 class Route {
+  /**
+   * Initialize the API route
+   * @param {string} method - HTTP method
+   * @param {string} path - URL path
+   */
   constructor(method, path) {
     this.method = method;
     this.path = path;
@@ -16,6 +24,12 @@ class Route {
     this.pathSegments = path.split('/');
   }
 
+  /**
+   * Validate that an HTTP request should trigger this API endpoint
+   * @param {string} requestMethod - HTTP method of a request
+   * @param {object} requestPathSegments - List of segments making up a URL path from a request
+   * @return {boolean} - True if this endpoint is triggered, false if not
+   */
   validateRequest(requestMethod, requestPathSegments) {
     if (requestMethod !== this.method || this.pathSegments.length !== requestPathSegments.length) {
       return false;
